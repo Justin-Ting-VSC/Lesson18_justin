384ace032fe566c7cb5eee9b307178df08cdf96d
Justin-Ting-VSC <24041609@myrp.edu.sg>
2026-01-15 11:45:49 +0800

    new fixs

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.listen(port, () => {
 app.get('/allcontact', async (req, res) => {
     try{
         let connection = await mysql.createConnection(dbConfig);
-        const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.contact');
         res.json(rows);
     } catch(err) {
         console.error(err);
